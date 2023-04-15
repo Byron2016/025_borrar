a5a03df1514bdf6eb29b988a0cc3462d45e62061
Byron <bgva2005@yahoo.com>
2023-04-15 04:53:53 -0500

    feat: index.ts modifications

--- dominicode/login_roles/src/index.ts
+++ dominicode/login_roles/src/index.ts
@@ -8,10 +8,22 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.listen(port);
+//app.listen(port);
 
 app.get("/", (req:Request, res:Response) => {
   res.send("pagina inicio");
 });
 
-console.log(`alisten on port ${port}`);
+app.get('/test', (_req: Request, res: Response) => {
+    return res.send('Express Typescript on Vercel')
+  })
+
+  app.get('/ping', (_req: Request, res: Response) => {
+    return res.send('pong 🏓')
+  })
+
+  app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`)
+  })
+
+//console.log(`alisten on port ${port}`);
