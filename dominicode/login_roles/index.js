891c9b5be2f005576f0e1c4e821c2749e949ab0a
Byron <bgva2005@yahoo.com>
2023-04-15 04:33:24 -0500

    feat: add route

--- dominicode/login_roles/index.js
+++ dominicode/login_roles/index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 4000;
 
 app.listen(port);
 
+app.get("/", (req, res) => {
+  res.send("pagina inicio");
+});
+
 console.log(`alisten on port ${port}`);
 console.log(`este es otro log ${port}`);
 console.log(`este es otro log ${port}`);
